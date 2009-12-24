9b245068ab8883fc1bf755a9ce8ab5d284a8c176
Mike C. Fletcher <mcfletch@vrplumber.com>
2009-12-23 20:04:01 -0500

    Json with comments example added.

---
+++ examples/jsonwcomments.py
@@ -0,0 +1,67 @@
+"""Simple example of a json parser with comment support"""
+from simpleparse.common import numbers, strings, comments
+
+declaration = r'''
+object    := ts,'{',!,ts,(member,sep?)*,'}'
+
+member    := string,ts,':',ts,value,ts
+array     := ts,'[',!,ts,(value,sep?)*,']',ts
+
+>value<     := js_string/object/array/true/false/null/float/int_w_exp/int
+
+int_w_exp := int,[eE],int
+
+>js_string< := string_single_quote/string_double_quote
+true      := 'true'
+false     := 'false'
+null      := 'null'
+
+<ts>      := ([ \t\n\r]+/comment)*
+<comment> := slashslash_comment/c_nest_comment
+<sep>     := (','?,ts)
+'''
+testData = """/* Bayeux protocol definition version 1.01, in language-        */
+{"test": {
+        'nums': [ 1,2.0,2e23,-2e-23,-2.3e+23 ],
+        'strings': [ "first", "second" ],
+        'string': "first",
+        'object': { },
+    },}
+"""
+biggertest = """
+
+"""
+from simpleparse.parser import Parser
+from simpleparse.dispatchprocessor import *
+import pprint
+
+class Processor( DispatchProcessor ):
+    def object( self, (tag,start,stop,children), buffer ):
+        obj = {}
+        for key,value in dispatchList( self, children, buffer ):
+            obj[key] = value
+        return obj
+    def member( self, (tag,start,stop,children), buffer ):
+        return dispatchList( self, children, buffer )
+    def array( self, (tag,start,stop,children), buffer ):
+        return dispatchList( self, children, buffer )
+    def int_w_exp( self, (tag,start,stop,(a,b)), buffer ):
+        base = dispatch( self, a, buffer )
+        exp = dispatch( self, b, buffer )
+        return base ** exp
+    int = numbers.IntInterpreter()
+    float = numbers.FloatInterpreter()
+    string = strings.StringInterpreter()
+    string_double_quote = strings.StringInterpreter()
+    string_single_quote = string_double_quote
+    def true( self, tag, buffer ):
+        return True
+    def false( self, tag, buffer ):
+        return False
+    def null( self, tag, buffer ):
+        return None
+
+
+parser = Parser( declaration, "object" )
+if __name__ =="__main__":
+    pprint.pprint( parser.parse( testData, processor=Processor()))
