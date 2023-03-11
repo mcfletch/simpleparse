279e5bc79584c00afd65efedbf4c5d28ae277e7e
Mike C. Fletcher <mcfletch@vrplumber.com>
2023-03-11 15:52:59 -0500

    UPDATE All supported releases have unicode support

--- simpleparse/stt/TextTools/mxTextTools/mxpyapi.h
+++ simpleparse/stt/TextTools/mxTextTools/mxpyapi.h
@@ -11,32 +11,21 @@
 
 */
 
-#if defined(PyUnicode_Check) && !defined(HAVE_UNICODE)
-# define HAVE_UNICODE
-#endif
-
-#if defined(HAVE_UNICODE) && !defined(Py_USING_UNICODE)
-# undef HAVE_UNICODE
-#endif
-
-#ifndef HAVE_UNICODE
-# undef PyUnicode_Check
-# define PyUnicode_Check(obj) 0
-#endif
+#define HAVE_UNICODE TRUE
 
 #if PY_MAJOR_VERSION >= 3
-#define PyInt_FromLong                  PyLong_FromLong
-#define PyInt_Check                     PyLong_Check
-#define PyInt_AS_LONG                   PyLong_AS_LONG
+#define PyInt_FromLong PyLong_FromLong
+#define PyInt_Check PyLong_Check
+#define PyInt_AS_LONG PyLong_AS_LONG
 
-#define PyString_FromStringAndSize      PyBytes_FromStringAndSize
-#define PyString_AsString               PyBytes_AsString
-#define PyString_FromString             PyBytes_FromString
-#define PyString_Check                  PyBytes_Check
-#define PyString_FromFormat             PyBytes_FromFormat
-#define PyString_GET_SIZE               PyBytes_GET_SIZE
-#define PyString_AS_STRING              PyBytes_AS_STRING
-#define _PyString_Resize                _PyBytes_Resize
+#define PyString_FromStringAndSize PyBytes_FromStringAndSize
+#define PyString_AsString PyBytes_AsString
+#define PyString_FromString PyBytes_FromString
+#define PyString_Check PyBytes_Check
+#define PyString_FromFormat PyBytes_FromFormat
+#define PyString_GET_SIZE PyBytes_GET_SIZE
+#define PyString_AS_STRING PyBytes_AS_STRING
+#define _PyString_Resize _PyBytes_Resize
 #endif
 
 /* EOF */
